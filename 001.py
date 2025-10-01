1c72d65d26b34334d0cbb1cc51c3b4982149c7f7
henriiiq46 <bernardohmg35@gmail.com>
2025-10-01 10:18:25 -0300

    Ryan

---
+++ 001.py
@@ -0,0 +1,9 @@
+ano_nascimento = int(input("Digite o ano em que você nasceu: "))
+ano_atual = 2025
+idade = ano_atual - ano_nascimento
+if idade == 18:
+    print("Você faz 18 anos neste ano!")
+elif idade > 18:
+    print("Você já fez 18 anos.")
+else:
+    print("Você ainda não fez 18 anos.")
